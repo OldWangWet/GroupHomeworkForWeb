c8f430bba0749c80a61f1c5bad4e5becd0551e03
Zhao Qi <johnwillson221013@icloud.com>
2024-12-01 20:55:07 +0800

    advertisement

---
+++ Advertisement/src/main/java/com/wst/advertisementv01/AdvertisementV01Application.java
@@ -0,0 +1,13 @@
+package com.wst.advertisementv01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdvertisementV01Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdvertisementV01Application.class, args);
+    }
+
+}
---
+++ Advertisement/src/test/java/com/wst/advertisementv01/AdvertisementV01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wst.advertisementv01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdvertisementV01ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
